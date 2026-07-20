4260cd7f8bbf56617d03fff29e9ae0bd12522970
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-20 10:23:19 +0700

    Progress : Study kasus menghitung konsumsi BBM

---
+++ src/LatihanOperator/MenghitungKonsumsiBBM.java
@@ -0,0 +1,16 @@
+package LatihanOperator;
+
+public class MenghitungKonsumsiBBM {
+    public static void main(String[] args){
+        int jarakTempuhMobil, konsumsiBBMMobil, hargaBensin;
+        jarakTempuhMobil = 420;
+        konsumsiBBMMobil = 14;
+        hargaBensin = 13000;
+
+        int totalBBMYangDibutuhkan = jarakTempuhMobil / konsumsiBBMMobil;
+        int totalBiayaBBM = hargaBensin * totalBBMYangDibutuhkan;
+
+        System.out.println("Bensin yang dibutuhkan: "+ totalBBMYangDibutuhkan+ "liter");
+        System.out.println("Total biaya BBM: Rp"+totalBiayaBBM);
+    }
+}
