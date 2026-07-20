0fe6c10622bbd5734c63ddbedce27d1f6d5d1dfc
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-20 10:53:46 +0700

    Progress: Belajar increment dan decrement

---
+++ src/LatihanOperator/MenggunakanIncrementdanDecrement.java
@@ -0,0 +1,13 @@
+package LatihanOperator;
+
+public class MenggunakanIncrementdanDecrement {
+    public static void main(String[] args){
+        int stokAwalToko = 20;
+        stokAwalToko++;
+        System.out.println("Stok akhir: "+ stokAwalToko);
+
+        stokAwalToko--;
+        stokAwalToko--;
+        System.out.println("Stok akhir: "+ stokAwalToko);
+    }
+}
