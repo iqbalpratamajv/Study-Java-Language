b21fc718531c28206e4b6dca51b29dc89498c3d4
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-20 10:16:02 +0700

    Progress: Study kasus menghitung umur

---
+++ src/LatihanOperator/MneghitungUmur.java
@@ -0,0 +1,12 @@
+package LatihanOperator;
+
+public class MneghitungUmur {
+    public static void main(String[] args){
+        int tahunSekarang, tahunLahir;
+        tahunSekarang = 2026;
+        tahunLahir = 2006;
+        
+        int umurSekarang = tahunSekarang - tahunLahir;
+        System.out.println("Umur kamu: "+ umurSekarang);
+    }
+}
