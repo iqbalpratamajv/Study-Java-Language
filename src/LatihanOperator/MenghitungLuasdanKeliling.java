f1f51a453c8bef8b3545b3a905ed4b27759e00bf
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-20 09:42:54 +0700

    Progres: Studi kasus menghitung luas dan keliling persegi panjang

---
+++ src/LatihanOperator/MenghitungLuasdanKeliling.java
@@ -0,0 +1,15 @@
+package LatihanOperator;
+
+public class MenghitungLuasdanKeliling {
+    public static void main(String[] args){
+
+        int panjangPersegiPanjang = 15;
+        int lebarPersegiPanjang = 8;
+
+        int totalLuas = panjangPersegiPanjang * lebarPersegiPanjang;
+        int totalKeliling = 2 * (panjangPersegiPanjang + lebarPersegiPanjang);
+
+        System.out.println("Total Luas: "+ totalLuas);
+        System.out.println("Total Keliling: "+ totalKeliling);
+    }
+}
