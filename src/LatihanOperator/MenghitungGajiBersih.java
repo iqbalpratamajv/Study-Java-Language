c8af56641bba88136eb03ef3a1c6b8fe7908a88a
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-20 09:49:29 +0700

    Progress: Study kasus Menghitung gaji bersih karyawan

---
+++ src/LatihanOperator/MenghitungGajiBersih.java
@@ -0,0 +1,16 @@
+package LatihanOperator;
+
+public class MenghitungGajiBersih {
+    public static void main(String[] args){
+        int gajiPokok, bonus, potongan;
+        gajiPokok = 5000000;
+        bonus = 750000;
+        potongan = 350000;
+
+        int totalGajiSebelumPotongan = gajiPokok + bonus;
+        int totalGajiSetelahPotongan = (gajiPokok + bonus) - potongan;
+
+        System.out.println("Total gaji sebelum dipotong: Rp"+ totalGajiSebelumPotongan);
+        System.out.println("Total gaji setelah potong: Rp"+ totalGajiSetelahPotongan);
+    }
+}
