764f2ee2c299804f3fc4290e2e35b259401da6e2
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-20 09:55:45 +0700

    Progress : Studi kasus menghitung nilai rata-rata siswa

---
+++ src/LatihanOperator/MenghitungNilaiRatarata.java
@@ -0,0 +1,16 @@
+package LatihanOperator;
+
+public class MenghitungNilaiRatarata {
+    public static void main(String[] args){
+        int nilaiMatematika, nilaiBahasaIndonesia, nilaiIPA;
+        nilaiMatematika = 85;
+        nilaiBahasaIndonesia = 90;
+        nilaiIPA = 78;
+
+        int totalNilaiSiswa = nilaiMatematika + nilaiBahasaIndonesia + nilaiIPA;
+        int nilaiRataRata = (nilaiMatematika + nilaiBahasaIndonesia + nilaiIPA) / 3;
+
+        System.out.println("Total nilai: "+totalNilaiSiswa);
+        System.out.println("Nilai rata rata: "+ nilaiRataRata);
+    }
+}
