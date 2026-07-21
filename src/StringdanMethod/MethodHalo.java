4bb41068647f199dd4d8fffdc5f4bc6586f78c87
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-21 14:25:04 +0700

    Progress: Belajar membuat method

---
+++ src/StringdanMethod/MethodHalo.java
@@ -0,0 +1,13 @@
+package StringdanMethod;
+
+public class MethodHalo {
+    public static void Hello(){
+        System.out.println("Hello");
+    }
+    
+    public static void main(String[] args) {
+        Hello();
+        Hello();
+        Hello();
+    }
+}
