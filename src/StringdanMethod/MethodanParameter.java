3d5db9473995efcb4858fa3de0e05ef81fa0b8fe
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-21 14:31:53 +0700

    Progress: Belajar method

---
+++ src/StringdanMethod/MethodanParameter.java
@@ -0,0 +1,12 @@
+package StringdanMethod;
+
+public class MethodanParameter {
+
+    public static void Sapa(String nama){
+        System.out.println("Hello,"+ nama);
+    }
+    public static void main(String[] args) {
+        Sapa("Jamal");
+        Sapa("Boni");
+    }
+}
