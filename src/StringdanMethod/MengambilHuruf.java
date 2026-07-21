b6a0b5895619e7cbae85031ee32d457c13889834
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-21 10:07:41 +0700

    Progress: Belajar mengambil sebuah huruf

---
+++ src/StringdanMethod/MengambilHuruf.java
@@ -0,0 +1,9 @@
+package StringdanMethod;
+
+public class MengambilHuruf {
+    public static void main(String[] args){
+        String nama = "iqbal dwi pratama";
+
+        System.out.println(nama.charAt(4));
+    }
+}
