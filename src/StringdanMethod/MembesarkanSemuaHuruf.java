1c18976985f9f241537da91ac1512cdecebcba26
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-21 10:03:13 +0700

    Progress: Belajar mengubah semua huruf menjadi besar

---
+++ src/StringdanMethod/MembesarkanSemuaHuruf.java
@@ -0,0 +1,9 @@
+package StringdanMethod;
+
+public class MembesarkanSemuaHuruf {
+    public static void main(String[] args){
+        String nama = "iqbal dwi pratama";
+
+        System.out.println(nama.toUpperCase());
+    }
+}
