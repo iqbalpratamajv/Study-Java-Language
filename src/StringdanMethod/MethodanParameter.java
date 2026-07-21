3a005302499c6dad0fbe28505f21a8f320b904d3
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-21 14:32:55 +0700

    Mengubah tata letak

--- src/StringdanMethod/MethodanParameter.java
+++ src/StringdanMethod/MethodanParameter.java
@@ -2,9 +2,10 @@ package StringdanMethod;
 
 public class MethodanParameter {
 
-    public static void Sapa(String nama){
-        System.out.println("Hello,"+ nama);
+    public static void Sapa(String nama) {
+        System.out.println("Hello," + nama);
     }
+
     public static void main(String[] args) {
         Sapa("Jamal");
         Sapa("Boni");
