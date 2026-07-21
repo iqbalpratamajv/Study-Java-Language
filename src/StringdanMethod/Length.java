ab97fda8122b63f64fe03b6e356ba83e37bc9ddf
iqbalpratamajv <iqbaldwi.pratama767@gmail.com>
2026-07-21 10:00:06 +0700

    Progres: Belajar lenght

---
+++ src/StringdanMethod/Length.java
@@ -0,0 +1,12 @@
+package StringdanMethod;
+
+public class Length {
+    public static void main(String[] args){
+        String namaSaya = "Iqbal";
+
+        System.out.println(namaSaya.length());
+    }
+}
+
+/*Length digunakan untuk menghitung jumlah kata
+pada sebuah kalimat*/
